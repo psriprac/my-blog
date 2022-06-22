2b33d7b1e56af6684aa6ace67e5cf768a9ae9a0f
psriprac <psriprac@gmail.com>
2022-06-21 22:45:37 -0700

    edit metadata

--- my-blog/gatsby-config.js
+++ my-blog/gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `CoderPat Blog`,
+    author: `Patrick Sriprachandr`,
+    description: `My personal blog.`,
+    siteUrl: `https://coderpat.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `https://twitter.com/patricksrip`,
     },
   },
   plugins: [
